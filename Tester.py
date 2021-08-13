2081ebc096b662b650231819d17260c0b6feae83
Muhammad Sholeh <60766389+Assyatier21@users.noreply.github.com>
2021-08-13 14:31:10 +0700

    Update tester.py

--- Tester.py
+++ Tester.py
@@ -3,11 +3,10 @@ import re
 query = input('Insert Query: ')
 word_query = query.split(" ")
 
-teks = "gua apa ya gataulah pokoknya gitu bisa ga ya adalah gini aja dah"
+teks = "this is the text in pattern search"
 
 for word in word_query:
-    # pola = re.compile("(?=(" + "|".join(map(re.escape, word_query)) + "))")
-    pola = re.compile(word)
+    pola = re.compile("(?=(" + "|".join(map(re.escape, word_query)) + "))")
     hasil = re.search(pola, teks)
     if hasil:
         print('Find')
